08d80e5b4f9c539cb4329dee6d20c05467003f44
Edgar Bernal <edgarbernal.ing@gmail.com>
2025-08-26 15:42:16 -0500

    Se agrega un arreglo y una función

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+let amigo = []; //Arreglo para almacenar los nombres
 
+function agregarAmigo() { // Función para implementar amigos
+    let nombre = document.getElementById('nombreAmigo').value;
+    amigo.push(nombre);
+    console.log(nombre);
+}
